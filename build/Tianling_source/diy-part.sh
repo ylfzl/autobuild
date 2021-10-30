85df4a4f7fa6d26e4a56923ea1fc5b16f8b120fc
281677160 <66651997+281677160@users.noreply.github.com>
2021-10-30 19:53:24 +0800

    Update diy-part.sh

--- build/Tianling_source/diy-part.sh
+++ build/Tianling_source/diy-part.sh
@@ -53,3 +53,11 @@ sed -i 's/"USB 打印服务器"/"打印服务"/g' `grep "USB 打印服务器" -r
 sed -i 's/"Web 管理"/"Web"/g' `grep "Web 管理" -rl ./`
 sed -i 's/"管理权"/"改密码"/g' `grep "管理权" -rl ./`
 sed -i 's/"Argon 主题设置"/"Argon设置"/g' `grep "Argon 主题设置" -rl ./`
+
+# 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间
+echo "CLEAR2=config.buildinfo" >> $GITHUB_ENV
+echo "CLEAR2=feeds.buildinfo" >> $GITHUB_ENV
+echo "CLEAR3=openwrt-x86-64-generic.manifest" >> $GITHUB_ENV
+echo "CLEAR3=openwrt-x86-64-generic-squashfs-rootfs.img.gz" >> $GITHUB_ENV
+echo "CLEAR2=sha256sums" >> $GITHUB_ENV
+echo "CLEAR3=version.buildinfo" >> $GITHUB_ENV
